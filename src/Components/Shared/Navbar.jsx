6ad47eab356deed617772cdca758b732af20a31b
unknown <morzmamun@gmail.com>
2023-11-06 15:16:14 +0600

    Login page done. user login with email and password also login with google

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,44 +1,81 @@
-
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../assets/logo.png'
+import useAuthProvider from "../../CustomHooks/useAuthProvider";
+import Swal from 'sweetalert2'
 
 const Navbar = () => {
+  const {logoutUser, user} = useAuthProvider()
+  const navLinks = <>
+    <li> <NavLink to={'/'}>Home</NavLink></li>
+    <li> <NavLink to={''}>Add Job</NavLink></li>
+    <li> <NavLink to={''}>My Posted Jobs</NavLink></li>
+    <li> <NavLink to={''}>My Bids</NavLink></li>
+    <li> <NavLink to={''}>Bid Requests</NavLink></li>
+    <li> <NavLink to={'/registration'}>Registration</NavLink></li>
+   
+  </>
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+  const handleLogoutUser = () => {
+    logoutUser()
+    .then(() => {
+      Toast.fire({
+        icon: 'success',
+        title: 'User Logout successfully'
+      })
+    })
+    .catch(() => {
+      Toast.fire({
+        icon: 'error',
+        title: 'User logout Unsuccessful.'
+      })
+    })
+  }
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-200 md:px-5">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40">
+       {navLinks}
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="w-6 h-6 md:w-10 md:h-10">
+        <img className="rounded-full" src={logo} alt="" />
+      </div>
+    <a className="btn btn-ghost normal-case md:text-xl">Dream Job</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {navLinks}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    {
+      user ? <div className="flex items-center">
+         <img className="w-10 h-10 rounded-full" src={user.photoURL} alt="" />
+        <p>{user.displayName}</p>
+        </div>: ''
+    }
+    {
+      user ? <button onClick={handleLogoutUser} className="btn  text-black btn-secondary">Logout</button> : 
+      <Link to={'/login'}><button className="btn  text-black btn-secondary">Login</button></Link>
+    }
+    
+   
   </div>
 </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 import {
@@ -9,6 +8,9 @@ import {
 } from "react-router-dom";
 import Main from './Main/Main.jsx';
 import Home from './Components/Pages/Home/Home.jsx';
+import AuthProvider from './Provider/AuthProvider.jsx';
+import Login from './Components/Pages/Login/Login.jsx';
+import Registration from './Components/Pages/Registration/Registration.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,7 +20,16 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>
-      }
+      },
+      {
+        path: '/registration',
+        element: <Registration></Registration>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      
     ]
   },
 ]);
@@ -26,6 +37,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
